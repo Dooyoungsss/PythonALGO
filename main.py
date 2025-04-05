5f76df19f2d022610d81d0bd9e4b2d2f19f8b875
Typhoon06 <wonhui0222@gmail.com>
2025-04-05 14:01:09 +0900

    Updated for turtle practice.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello World')
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+turtle.done()
